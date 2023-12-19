ecfe182eb4ea7243dd106182a83ed94f4d9545c3
rwayf001 <fezisa.rwayi@vcontractor.co.za>
2023-12-20 00:38:50 +0200

    Artifact and refinement of assertions

--- cypress/e2e/AutomationPrac.cy.js
+++ cypress/e2e/AutomationPrac.cy.js
@@ -8,11 +8,11 @@ describe('AutomationPrac', () => {
   })
 
 
-  it('TC1 - Validate that only 1 radio button is checked - when	clicking on a radio button ', () => {
+  it('TC1 - Validate that only 1 radio button is checked - when	clicking on a radio button', () => {
 
     // Click on radio button 3 
     cy.get('#radio-btn-example fieldset [value="radio3"]').click()
-    //cy.wait(1000)
+    
 
     // Validate that only 1 radio button is checked
     cy.get('#radio-btn-example fieldset [value="radio3"]').should('be.checked')
@@ -21,7 +21,7 @@ describe('AutomationPrac', () => {
 
     // Click on radio button 2 
     cy.get('#radio-btn-example fieldset [value="radio2"]').click()
-    //cy.wait(1000)  
+  
 
     // Validate that only 1 radio button is checked
     cy.get('#radio-btn-example fieldset [value="radio2"]').should('be.checked')
@@ -30,37 +30,42 @@ describe('AutomationPrac', () => {
   })
 
 
-  it('TC2 - Check that selected item is displayed - when partially spelt and selected from "Suggestion Class" list', () => {
+  it('TC2 - Check that selected item is displayed - when partially spelt then selected from the "Suggestion Class" list', () => {
 
     // Type 'South' in the text field 
     cy.get('#select-class-example fieldset #autocomplete').type('South')
-
+    
 
     // Select 'South Africa' from the list
     cy.get('ul#ui-id-1').should('be.visible').get('.ui-menu-item').contains('South Africa').click()
+   
 
-    // Check "South Africa cy.get('ul#ui-id-1').should('be.visible').get('.ui-menu-item').contains('South Africa').click()"
+    // Check "South Africa" is in the textbox
     cy.get('#select-class-example fieldset #autocomplete').should('have.value', 'South Africa')
-
+  
 
     // Clear text field
     cy.get('#select-class-example fieldset #autocomplete').type('{selectAll}{backspace}')
+   
 
     // Type 'Republic' in the text field  
     cy.get('#select-class-example fieldset #autocomplete').type('Republic')
+   
 
     // Select First option from the list
     cy.get('ul#ui-id-1').should('be.visible').get('.ui-menu-item:first-child').click()
+    
 
   })
 
 
-  it('TC3 - Validate that all checkboxes are checked - when all checkboxes are checked ', () => {
+  it('TC3 - Validate that all checkboxes are checked - when all the checkboxes are checked', () => {
 
     // Check all the checkboxes
     cy.get('fieldset label #checkBoxOption1').check()
     cy.get('fieldset label #checkBoxOption2').check()
     cy.get('fieldset label #checkBoxOption3').check()
+   
 
     // Validate checkboxes are checked
     cy.get('fieldset label #checkBoxOption1').should('be.checked')
@@ -77,6 +82,7 @@ describe('AutomationPrac', () => {
 
     // Uncheck the first checkbox
     cy.get('fieldset label #checkBoxOption1').uncheck()
+  
 
     // Validate remaining checkboxes remain checked
     cy.get('fieldset label #checkBoxOption2').should('be.checked')
@@ -86,8 +92,11 @@ describe('AutomationPrac', () => {
 
   it('TC5 - Validate that an element is hidden - when the "hide" button is clicked', () => {
 
+    cy.get('fieldset [onclick="showElement()"]').click()
+   
     // Click on the "hide" button
     cy.get('fieldset [onclick="hideElement()"]').click()
+ 
 
     // Validate element is hidden
     cy.get('fieldset [name="show-hide"]').should('not.be.visible')
@@ -96,29 +105,34 @@ describe('AutomationPrac', () => {
 
   it('TC6 - Validate that an element is shown - when the "show" button is clicked', () => {
 
+    cy.get('fieldset [onclick="hideElement()"]').click()
+    
+
     // Click on the "show" button
     cy.get('fieldset [onclick="showElement()"]').click()
+
 
     // Validate element is shown
     cy.get('fieldset [name="show-hide"]').should('be.visible')
   })
 
 
-  it('TC7 - Validate the Amount = 46 AND matches the Name, Position and City in the Web Table', () => {
+  it('TC7 - Validate on the Web Table that the Amount = specified number AND matches the Name, Position and City', () => {
 
     // Validate the Amount = 46 AND matches the Name, Position and City 
     cy.get('#product tbody tr:nth-child(6)').should('contain', 'Joe').should('contain', 'Postman').should('contain', 'Chennai').should('contain', '46')
-
+    
   })
 
-  it('TC8 - Validate that the total amount collected in the Web Table = 296', () => {
+  it('TC8 - Validate on the Web Table that the total amount collected = 296', () => {
 
     // Validate that the total amount collected in the Web Table = 296
     cy.get('.totalAmount').should('contain', '296')
+   
 
   })
 
-  it('TC9 - Validate that the sum of all the rows in the Web Table is correct', () => {
+  it('TC9 - Validate that the sum of all the Amounts in the Web Table is correct', () => {
 
     // still investigating below coding issue due to 'sum' variable not bringing the desired data outside for loop, 
     // function works perfectly this can be tested with a console log but for some reason variable 'sum' will not retain data from in the for loop.
@@ -138,6 +152,7 @@ describe('AutomationPrac', () => {
 
 
     cy.log(sum)
+  
 
   })
 
@@ -146,6 +161,7 @@ describe('AutomationPrac', () => {
 
     // Validate that iFrame exists on the page
     cy.get('#courses-iframe').should('be.visible')
+ 
 
   })
 
@@ -161,6 +177,7 @@ describe('AutomationPrac', () => {
         .click()
 
     })
+    
 
   })
 
